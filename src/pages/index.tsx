91d72a984c4524d4057f8d0cea44df2978c9865b
pratamaizzat <pratama.izzat231298@gmail.com>
2023-10-26 23:37:32 +0700

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "~/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <Button className="bg-red-500 hover:bg-red-400">This is CN Button</Button>
+    </main>
+  );
+}
